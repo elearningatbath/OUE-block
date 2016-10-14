86f19e0f27ad14d58c52a841178764e1cc8aa2c6
Hittesh Ahuja <ha386@bath.ac.uk>
2016-10-14 15:49:19 +0100

    Bumped up version Added dependency

--- blocks/bath_oue/version.php
+++ blocks/bath_oue/version.php
@@ -25,6 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013092500;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011112900;        // Requires this Moodle version
+$plugin->version = 2016080900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires = 2011112900;        // Requires this Moodle version
 $plugin->component = 'block_bath_oue'; // Full name of the plugin (used for diagnostics)
+$plugin->dependencies = array(
+    'enrol_samisv2' => ANY_VERSION
+);
