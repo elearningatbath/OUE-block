acdd41dc63f9b7b29463ddb270256fe467f42f86
Hittesh Ahuja <ha386@bath.ac.uk>
2016-10-14 15:48:42 +0100

    Connecting to new SAMIS

--- blocks/bath_oue/stats.php
+++ blocks/bath_oue/stats.php
@@ -1,53 +1,48 @@
 <?php
- 
+
 define('AJAX_SCRIPT', true);
 require_once('../../config.php');
 require_once($CFG->libdir . '/datalib.php');
-require_once($CFG->dirroot . '/local/sits/lib/sits_client_request.class.php');
-$username = optional_param('username',NULL, PARAM_ALPHANUM);
-	//Fun stuff!
-	$report = new report();
+require_once('classes/samis_oue.php');
+$username = optional_param('username', NULL, PARAM_ALPHANUM);
+//Fun stuff!
 
-		try{
-			$sits = new sits($report);
-		}
-		catch(\Exception $e){
-			$result['sits_error'] = true;
-			echo json_encode($result);
-			die();
-		}
-		$student = new stdClass;
-		$student->username = $username;
-		if(!$result = $sits->get_survey_stats_student($student))
-		{
-			$result['result_error'] = true;	
-		}
-		else{
-				$result['data'] = explode("/",$result[0]); // Remove tabs
-				if(is_array($result['data']))
-				{
-					if($result['data'][0] == ''){
-						$result['data'][0] = 'NO ROWS'; // Fix for #1062
-					}
-					if($result['data'][0] !== 'NO ROWS')
-					{
-						foreach($result['data'] as $key => $value)
-						{
-							unset($result['data'][$key]);
-							$newkey = substr($value,0,1);
-							if($value === '')
-							{
-								$newkey = 'S';
-							}
-							$result['data'][$newkey] = (int) substr($value,1);
-						}
-					}
-					else{
-						$result['data'] = $result['data'][0];
-					}				
-				}
-			unset($result[0]);
-		}
+
+try {
+    //$sits = new sits($report);
+    $samis_oue = new \samis_oue($username);
+} catch (\Exception $e) {
+    var_dump($e);
+    $result['sits_error'] = true;
+    echo json_encode($result);
+    die();
+}
+$student = new stdClass;
+$student->username = $username;
+if (!$result = $samis_oue->get_survey_stats_student($student)) {
+    $result['result_error'] = true;
+} else {
+    $result['data'] = explode("/", $result[0]); // Remove tabs
+    if (is_array($result['data'])) {
+        if ($result['data'][0] == '') {
+            $result['data'][0] = 'NO ROWS'; // Fix for #1062
+        }
+        if ($result['data'][0] !== 'NO ROWS') {
+            foreach ($result['data'] as $key => $value) {
+                unset($result['data'][$key]);
+                $newkey = substr($value, 0, 1);
+                if ($value === '') {
+                    $newkey = 'S';
+                }
+                $result['data'][$newkey] = (int)substr($value, 1);
+            }
+        } else {
+            $result['data'] = $result['data'][0];
+        }
+    }
+    unset($result[0]);
+}
+$result['data'] = array('S' => 3, 'C' => 2);
 echo json_encode($result);
 		
 
